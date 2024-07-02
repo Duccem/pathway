d94007860e629bea27abe549316f4bbaa72dcfba
Jose Veliz <ducen29@gmail.com>
2024-07-01 22:07:45 -0400

    The image

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div className="grid grid-cols-1 sm:grid-cols-2 w-screen h-screen">
       <div className="h-full w-full max-sm:hidden flex justify-center items-center">
         <div className="flex flex-col justify-between items-center max-w-[400px]">
-          <img src="/images/illustration.png" alt="" className="h-[400px] aspect-square" />
+          <img src="https://s2ry7adukcrpchlb.public.blob.vercel-storage.com/Illustration-gNY621pupq57idt9akiZwyWNe6BURB.png" alt="" className="h-[400px] aspect-square" />
           <h1 className="text-3xl font-semibold text-center mb-5">Transform your Phone into a Classroom</h1>
           <p className='text-sm font-medium text-center'>Seize the moment and help shape the future by starting a career in blockchain now</p>
         </div>
